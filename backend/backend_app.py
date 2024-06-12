f2ec2cc0f9753498e43099660377ff5575f39f29
Mami K <49592804+carveler@users.noreply.github.com>
2024-06-12 14:56:48 +0200

    feat: add endpoint to add post

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,10 +10,31 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET'])
+def validate_post_data(data):
+    if "title" not in data:
+        return "title"
+    elif "content" not in data:
+        return "content"
+    return True
+
+
+@app.route("/api/posts", methods=["GET", "POST"])
 def get_posts():
+    if request.method == "POST":
+        post = request.get_json()
+        if not validate_post_data(post) == True:
+            return (
+                jsonify(
+                    {"error": f"Invalid post data, {validate_post_data(post)} is missing"}
+                ),
+                400,
+            )
+        post["id"] = max(post["id"] for post in POSTS) + 1
+        POSTS.append(post)
+        return jsonify(post), 201
+
     return jsonify(POSTS)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
